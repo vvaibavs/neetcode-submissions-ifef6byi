e4390b1e4ec4a05ec7884210648c7cd6f52e7938
Vaibav Subramanian <84254719+vvaibavs@users.noreply.github.com>
2026-05-14 18:21:39 -0500

    Add: three-integer-sum - submission-7

---
+++ Data Structures & Algorithms/three-integer-sum/submission-7.py
@@ -0,0 +1,15 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+        res = []
+        for n in range(len(nums)):
+            remainder = {}
+            target = -(nums[n])
+            for i in range(len(nums)):
+                if i!=n:
+                    if target - nums[i] not in remainder:
+                        remainder[nums[i]] = i
+                    else:
+                        if sorted([nums[n], target-nums[i], nums[i]]) not in res:
+                            res.append(sorted([nums[n], target-nums[i], nums[i]]))
+        return res

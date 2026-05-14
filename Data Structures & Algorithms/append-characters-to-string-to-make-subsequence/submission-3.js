34a98b1957238b665feeceb4ff769ca22a89e5bc
Vaibav Subramanian <84254719+vvaibavs@users.noreply.github.com>
2026-05-13 20:28:17 -0500

    Add: append-characters-to-string-to-make-subsequence - submission-3

---
+++ Data Structures & Algorithms/append-characters-to-string-to-make-subsequence/submission-3.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {number}
+     */
+    appendCharacters(s, t) {
+        let k = 0;
+        for (let i = 0; i < s.length; i++) {
+            if (s[i] === t[k]) k++;
+        }
+        return t.length - k;
+    }
+}
